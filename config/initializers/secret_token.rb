435236c2c898f2a3a76184b1260d946c7f293282
Dave Evans <devans725@hotmail.com>
2013-07-04 12:11:01 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WageApp::Application.config.secret_key_base = 'f5cec06173c37c4d3b633f02417be522451ddbb35ad41852a7feeb6908a5eee7be7700612aaa61eecdc31e24c587d93609c81a2579bbcde87ae340d879862bc8'
